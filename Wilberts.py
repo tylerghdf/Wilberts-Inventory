ea2143330d3864a809f87ac1efee939b43dcce93
Tyler <57166511+tylerghdf@users.noreply.github.com>
2024-03-14 01:29:54 -0400

    Update Wilberts.py
    Reworked for their new site design.
    Simple UI added.
    Identifying changes crudely implemented.
    
    Need to add error handling for file not found when finding changes

--- Wilberts.py
+++ Wilberts.py
@@ -1,73 +1,54 @@
 import constants
 import os
+import time
+import json
 import requests
+import tkinter as tk
 import pandas as pd
-from bs4 import BeautifulSoup
+from pandastable import Table
 
-def get_makes():
-    url = 'https://www.wilberts.com/u-pull-it/'
+def parse_cars(raw_data):
+    raw_data = json.loads(raw_data)
 
-    request_car_makes = requests.post(url)
+    # Only the details field holds the information needed (Never empty)
+    details = raw_data['details']
 
-    if request_car_makes.status_code == 200:
-        makes_soup = BeautifulSoup(request_car_makes.text, 'html.parser')
-        makes_tag = makes_soup.find(id='search_car_makes')
+    # Load nested json and get cars from vehicle details field (Empty is no cars of make a model)
+    cars_data = json.loads(details)
+    cars_data = cars_data['vehicleDetails']
 
-        car_makes = {}
+    # Cleanest way to avoid errors
+    if len(cars_data) == 0:
+        return cars_data
 
-        for make in makes_tag:
-            car_makes[make['value']] = make.text
+    # Empty list for cleaned car data
+    cleaned_car_data = []
 
-        del car_makes['0']
+    for car in cars_data:
+        car = {
+        'Make': car['Make'],
+        'Model': car['Model'],
+        'Year': car['Year'],
+        'VIN': car['VIN'],
+        'Days in Yard': car['DaysInYard'],
+        'StockID': car['StockID']
+        }
 
-        return car_makes
-    else:
-        print('Makes request failed!')
+        cleaned_car_data.append(car)
 
-def get_models(make_id):
-    url = 'https://www.wilberts.com/u-pull-it/wp-admin/admin-ajax.php'
+    return cleaned_car_data
 
+def get_cars(make, model):
+    # You only need these 2 fields for the payload
     payload = {
-    'action': 'genInvCarModels',
-    'carmake': make_id,
-    'carmodel': '248',
-    'caryear': '2009',
-    'currentlocation': 'bath'
+    'Make': make,
+    'Model': model
     }
 
-    request_car_models = requests.post(url, data=payload)
-
-    if request_car_models.status_code == 200:
-        models_soup = BeautifulSoup(request_car_models.text, 'html.parser')
-
-        car_models = {}
-
-        for model in models_soup:
-            car_models[model['value']] = model.text
-
-        del car_models['0']
-
-        return car_models
-    else:
-        print('Models request failed!')
-
-def get_cars(make_id, model_id):
-    url = 'https://www.wilberts.com/u-pull-it/wp-admin/admin-ajax.php'
-
-    payload = {
-    'action': 'genInvSearchReport',
-    'carmake': make_id,
-    'carmodel': model_id,
-    'caryear': '2009',
-    'currentlocation': 'bath'
-    }
-
-    request_cars = requests.post(url, data=payload)
+    request_cars = requests.post(constants.CARS_URL, json=payload)
 
     if request_cars.status_code == 200:
-        cars_soup = BeautifulSoup(request_cars.text, 'html.parser')
-
-        return cars_soup
+        return request_cars.text
     else:
         print('Cars request failed!')
 
@@ -77,46 +58,54 @@ def save_cars():
     'Model': [],
     'Year': [],
     'VIN': [],
-    'Days in Yard': []
+    'Days in Yard': [],
+    'StockID': []
     }
     
-    makes = get_makes()
+    # The values are what's used in the requests, but keep the keys/ids around for later possible checking.
+    makes = constants.MAKES_DICT.values()
 
-    for make_id, make_text in makes.items():
-        models = get_models(make_id)
+    for make in makes:
+        models = constants.MODELS_DICT[make]
 
-        for model_id, model_text in models.items():
-            cars = get_cars(make_id, model_id)
+        for model in models:
+            cars = get_cars(make, model) # Don't mess with this string. It has so much bad formatting that touching it breaks everything json.
 
-            for car in cars:
-                year = car.contents[0].text
-                vin = car.contents[2].text
-                days = car.contents[3].text
+            parsed_cars = parse_cars(cars)
+            
+            for car in parsed_cars:
+                car_info['Make'].append(car['Make'])
+                car_info['Model'].append(car['Model'])
+                car_info['Year'].append(car['Year'])
+                car_info['VIN'].append(car['VIN'])
+                car_info['Days in Yard'].append(car['Days in Yard'])
+                car_info['StockID'].append(car['StockID'])
 
-                if year != 'Year':
-                    car_info['Make'].append(make_text)
-                    car_info['Model'].append(model_text)
-                    car_info['Year'].append(year)
-                    car_info['VIN'].append(vin)
-                    car_info['Days in Yard'].append(days)
+                print(car)
+            
+            # Rate limit out of respect :) ILY Wilberts!
+            time.sleep(.3)
 
     # Converting data to dataframe
     yard_data = pd.DataFrame(car_info)
 
-    #Cleaning data and saving (formats for comparisons)
+    # Cleaning data and saving (formats for comparisons)
     yard_data = yard_data.dropna()
-
-    if not os.path.exists('Data'):
-        os.mkdir('Data')
+    yard_data = yard_data.drop_duplicates(subset='VIN')
 
     if os.path.exists('Data/yard_data.csv'):
         os.rename('Data/yard_data.csv', 'Data/old_yard_data.csv')
 
-    yard_data.to_csv('Data/yard_data.csv', index=False)
+        yard_data.to_csv('Data/yard_data.csv', index=False)
+
+        get_changes()
+    else:
+        yard_data.to_csv('Data/yard_data.csv', index=False)
 
 # Comparison of new and old yard data
 def get_changes():
     print('Hey dummy make this actually take into account the time since last scraped so it does the thing right')
+    print('Also make this do something if neither file exists')
     new_data = pd.read_csv('Data/yard_data.csv')
     old_data = pd.read_csv('Data/old_yard_data.csv')
 
@@ -125,15 +114,39 @@ def get_changes():
     #Saving all the changes to respective files
     changed_data[changed_data['Days in Yard'] < 7].to_csv('Data/added_to_yard.csv', index=False)
     changed_data[changed_data['Days in Yard'] > 7].to_csv('Data/removed_from_yard.csv', index=False)
-    
 
+def display_yard_data():
+    yard_data = pd.read_csv('Data/yard_data.csv')
+
+    data_window = tk.Toplevel()
+    data_window.title('Yard Data')
+
+    car_table = Table(data_window, dataframe=yard_data)
+    car_table.show()
+
+def display_new_data():
+    yard_data = pd.read_csv('Data/added_to_yard.csv')
+
+    data_window = tk.Toplevel()
+    data_window.title('New Yard Data')
+
+    car_table = Table(data_window, dataframe=yard_data)
+    car_table.show()
 
 def main():
-    #Urllib parser has dumb interpretation on their robots file. I don't like it either...
-    if 'Allow: /wp-admin/admin-ajax.php' in requests.get('https://www.wilberts.com/robots.txt').text:
-        save_cars()
-    else:
-        print('No more robots!')
+    main_window = tk.Tk()
+    main_window.title('Wilberts Car Data')
+
+    search_button = tk.Button(main_window, text='Run Search', command=save_cars)
+    search_button.pack()
+
+    yard_data_button = tk.Button(main_window, text='Display Yard Data', command=display_yard_data)
+    yard_data_button.pack()
+
+    new_data_button = tk.Button(main_window, text='Display New Data', command=display_new_data)
+    new_data_button.pack()
+
+    main_window.mainloop()
 
 if __name__ == '__main__':
     main()
